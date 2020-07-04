95ee909f14930f99e8c889594d685eebe73e8be9
frfontoura <frfontoura@gmail.com>
2020-07-04 16:44:47 -0300

    Criaçao do datePicker na tela de agendamento

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -37,7 +37,7 @@ const Dashboard: React.FC = () => {
     api.get('providers').then((response) => {
       setProviders(response.data);
     });
-  }, [signOut]);
+  }, []);
 
   const navigateToProfile = useCallback(() => {
     // navigate('Profile');
